e2abf441b63d3e9d09f5856e1e5e47d9d537d9c5
satoshitodaka <satoshitodaka0705@gmail.com>
2022-08-18 16:46:35 +0900

    add ユーザーの詳細情報の表示画面を実装

---
+++ pages/users/[uid].tsx
@@ -0,0 +1,39 @@
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { User } from '../../models/User'
+import { collection, doc, getDoc, getFirestore } from 'firebase/firestore'
+
+type Query = {
+  uid: string
+}
+
+export default function UserShow() {
+  const [user, setUser] = useState<User>(null)
+  const router = useRouter()
+  const query = router.query as Query
+  console.log(query)
+
+  // useEffectを利用し、初回のみ実行させるようにする
+  useEffect(() => {
+    if (query.uid === undefined) {
+      return
+    }
+
+    async function loadUser() {
+      const db = getFirestore()
+      const ref = doc(collection(db, 'users'), query.uid)
+      const userDoc = await getDoc(ref)
+
+      if (!userDoc.exists()) {
+        return
+      }
+
+      const gotUser = userDoc.data() as User
+      gotUser.uid = userDoc.id
+      setUser(gotUser)
+    }
+    loadUser()
+  }, [query.uid])
+
+  return <div>{user ? user.name : 'ロード中…'}</div>
+}
